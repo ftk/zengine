1f5113bedb308e0242b070c87ab5a4e3a4d83332
ftk <ftk@users.noreply.github.com>
2017-03-02 18:22:40 +0300

    logging improved

---
+++ util/log.cpp
@@ -0,0 +1,39 @@
+//
+// Created by fotyev on 2017-03-01.
+//
+
+#include "log.hpp"
+
+#include <iostream>
+
+#include <ctime>
+
+namespace log_detail {
+std::vector<logger> loggers{{std::cerr, log_level::debug2, 0}};
+
+
+
+
+void log_start(const logger& l)
+{
+    if(l.flags & TIME)
+    {
+        time_t stamp = time(nullptr);
+        struct tm * timeinfo = localtime(&stamp);
+        char buffer[32];
+        strftime(buffer, sizeof(buffer), "[%T] ", timeinfo);
+        l.s << buffer;
+    }
+}
+
+void log_end(const logger& l)
+{
+    // end of line
+    if(!(l.flags & NO_NEWLINE))
+        l.s << '\n';
+    if(!(l.flags & NO_FLUSH))
+        l.s.flush();
+}
+
+} //namespace log_detail
+
--- util/log.hpp
+++ util/log.hpp
@@ -5,9 +5,14 @@
 #ifndef ZENGINE_LOG_HPP
 #define ZENGINE_LOG_HPP
 
-#include <iostream>
+#include <ostream>
 
-static constexpr auto& log_stream = std::cerr;
+#ifdef LOG_STATIC
+#include <iostream>
+#else
+
+#include <vector>
+#endif
 
 enum class log_level
 {
@@ -22,38 +27,57 @@ enum class log_level
     debug5 = 65,
     all = 100,
 };
-constexpr log_level cur_level = log_level::debug2;
 
-namespace detail {
-template <typename T>
-inline void log_arg(log_level level, T&& t)
+namespace log_detail {
+
+enum // flags
 {
-    log_stream << std::forward<T>(t) << ' ';
+    NO_NEWLINE = 1u << 0,
+    NO_FLUSH = 1u << 1,
+    TIME = 1u << 2,
+
+};
+
+struct logger
+{
+    std::ostream& s;
+    log_level max;
+    unsigned flags;
+};
+
+#ifdef LOG_STATIC
+#ifndef LOG_STATIC_LOGGERS
+#define LOG_STATIC_LOGGERS { std::cerr, log_level::debug2, 0 }
+#endif
+static constexpr logger loggers[] { LOG_STATIC_LOGGERS };
+#else
+extern std::vector<logger> loggers;
+#endif
+
+template <typename T>
+inline void log_arg(const logger& l, T&& t)
+{
+    l.s << std::forward<T>(t) << ' ';
 }
 // specializations?
 
 
-inline void log_start(log_level level) {}
+void log_start(const logger& l);
+void log_end(const logger& l);
 
-inline void log_args(log_level level)
-{
-}
+
+inline void log_args(const logger& l) { }
 
 template <typename Arg1, typename... Args>
-inline void log_args(log_level level, Arg1&& arg1, Args&& ... args)
+inline void log_args(const logger& l, Arg1&& arg1, Args&& ... args)
 {
-    detail::log_arg(level, std::forward<Arg1>(arg1));
-    log_args(level, std::forward<Args>(args)...);
+    log_arg(l, std::forward<Arg1>(arg1));
+    log_args(l, std::forward<Args>(args)...);
 
 }
 
-inline void log_end(log_level level)
-{
-    // end of line
-    log_stream << std::endl;
-}
 
-} // namespace detail
+} // namespace log_detail
 
 
 
@@ -61,13 +85,19 @@ inline void log_end(log_level level)
 template <typename... Args>
 inline void logger(log_level level, Args&&... args)
 {
-    if(level <= cur_level)
+    using namespace log_detail;
+    for(const auto& l : loggers)
     {
-        detail::log_start(level);
-        detail::log_args(level, std::forward<Args>(args)...);
-        detail::log_end(level);
+        if(level <= l.max)
+        {
+            log_start(l);
+            log_args(l, std::forward<Args>(args)...);
+            log_end(l);
+        }
     }
 };
+
+inline auto& loggers() { return log_detail::loggers; }
 
 #define LOGGER(level,...) logger(log_level:: level, ##__VA_ARGS__)
 
