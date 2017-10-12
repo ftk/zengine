fda281ad6042344d53dba0e5c05be860f1cd05ce
ftk <ftk@users.noreply.github.com>
2017-10-12 14:16:43 +0300

    removed buggy ptr, fixed collision checking, added script cbs... first semi-working "game"?

--- examples/zwarudo/controller.hpp
+++ examples/zwarudo/controller.hpp
@@ -97,31 +97,29 @@ class player
     static constexpr qvm::vec2 size{0.05, 0.05};
 public:
 
-    std::shared_ptr<world> w;
-
     void draw()
     {
         auto& texture = g_app->textures->get_from_file("resources/zee.png");
         g_app->window->render.copy2(texture, pos, size);
     }
 
-    void update(tick_t tick)
+    void update(tick_t tick, world& w)
     {
         using namespace qvm;
         pos += vel;
-        // gravity
 
         // friction...
-        vel *= 0.95;
+        vel *= 0.96;
 
-        vel += vec2{0,-0.0003};
+        // gravity
+        vel += vec2{0,-0.0005};
 
 
         // stop
-        if(w->solid(pos))
+        if(w.solid(pos) || w.solid(pos + size) || w.solid(pos + X0(size)) || w.solid(pos + _0Y(size)))
             vel = vec2{0,0};
 
-        if(dir && abs(X(vel)) < 0.003)
+        if(dir && abs(X(vel)) < 0.004)
             X(vel) += dir / 400.f;
 
     }
@@ -142,7 +140,7 @@ public:
     template <typename T>
     void operator()(T event) {}
 
-    SERIALIZABLE((pos)(vel)(w))
+    SERIALIZABLE((pos)(vel)(dir))
 };
 
 
@@ -150,8 +148,7 @@ class zwstate
 {
 public:
     std::unordered_map<net_node_id, player> players;
-    std::shared_ptr<world> w = std::make_shared<world>();
-
+    world w;
 
     zwstate()
     {
@@ -160,7 +157,7 @@ public:
     void update(tick_t tick)
     {
         for(auto& p : players)
-            p.second.update(tick);
+            p.second.update(tick, w);
     }
 
     void draw()
@@ -168,7 +165,7 @@ public:
         for(auto& p : players)
             p.second.draw();
         // draw world
-        w->draw();
+        w.draw();
     }
 
     void on_input(const tick_input_t& input)
@@ -181,7 +178,6 @@ public:
         {
             // new player
             players.insert({input.player, player{}});
-            players[input.player].w = w;
         }
         else if(input.event.which() == event::player_leave::index)
         {
@@ -189,22 +185,19 @@ public:
         }
         else if(const event::click * ev = boost::get<event::click>(&input.event))
         {
-            w->flip({ev->x, ev->y});
+            w.flip({ev->x, ev->y});
         }
     }
 
     SERIALIZABLE((players)(w))
-
-    void operator=(const zwstate& rhs)
-    {
-        *w = *rhs.w; players = rhs.players;
-    }
 };
 
 /*< register_module(name=>'controller', class=>'controller', scriptexport=>[qw(join host stop)]); >*/
 class controller : public basic_module
 {
     multiplayer_game<gamestate_simulator2<zwstate>> game {g_app->netgame.get()};
+
+    decltype(game.state) prevstate = game.state;
 
 public:
     void join(net_node_id remote)
@@ -226,6 +219,8 @@ public:
 
     bool on_event(const SDL_Event& ev) override
     {
+        if(!game.playing())
+            return true;
         switch(ev.type)
         {
             case SDL_KEYDOWN:
@@ -248,6 +243,10 @@ public:
                     case SDL_SCANCODE_D:
                         game.push_event(event::movement{1, 0, down});
                         return false;
+                    case SDL_SCANCODE_ESCAPE:
+                        game.stop();
+                        return false;
+
                     default:
                         break;
                 }
@@ -255,7 +254,7 @@ public:
             }
             case SDL_MOUSEBUTTONDOWN:
             {
-                if(ev.button.state == SDL_PRESSED)
+                if(ev.button.state == SDL_PRESSED && ev.button.button == 3)
                 {
                     auto pos = g_app->window->to_gl_coords({ev.button.x, ev.button.y});
                     //=- register_event(name=>'click', params=>[[qw(float x)], [qw(float y)]]);
@@ -272,6 +271,33 @@ public:
     {
         game.sim.draw();
         game.update();
+
+        auto newstate = game.state;
+
+        if(prevstate != newstate)
+        {
+            switch(newstate)
+            {
+                case decltype(game)::CLIENT:
+                    {
+                        //=- register_callback('void ()', 'on_connect');
+                        g_app->script->on_connect();
+                    }break;
+                case decltype(game)::STOPPED:
+                    {
+                        //=- register_callback('void ()', 'on_disconnect');
+                        g_app->script->on_disconnect();
+                    }break;
+                case decltype(game)::HOST:
+                    {
+                        //=- register_callback('void ()', 'on_host');
+                        g_app->script->on_host();
+                    }break;
+                default: break;
+            }
+        }
+
+        prevstate = newstate;
     }
 
 };
