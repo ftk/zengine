417b4d7a9514afc5770dc39c8876ea12c5d9720d
ftk <ftk@users.noreply.github.com>
2018-11-13 20:11:53 +0300

    signals

--- src/util/signals.hpp
+++ src/util/signals.hpp
@@ -15,27 +15,103 @@ namespace sig = boost::signals2;
 #include <functional>
 #include <vector>
 
+#include "util/assert.hpp"
+
 namespace sig {
     template <typename R, typename... Args>
-    class signal;
+    class basic_signal;
 
     template <typename R, typename... Args>
-    class signal<R(Args...)> : std::vector<std::function<R(Args...)>>
+    class basic_signal<R(Args...)> : public std::vector<std::function<R(Args...)>>
     {
     public:
+        void operator ()(Args... args)
+        {
+            for(auto& f : *this)
+                f(args...);
+        }
+
         template <typename F>
         void connect(F&& f)
         {
             this->push_back(std::forward<F>(f));
         }
+    };
 
-        void operator ()(Args... args)
+    struct connection
+    {
+        std::function<void ()> disconnector;
+
+        void disconnect() const noexcept
         {
-            for(auto& f : (*this))
-                f(args...);
+            disconnector();
         }
     };
 
+    class scoped_connection : private connection
+    {
+    public:
+        scoped_connection(connection c) : connection::connection(std::move(c)) {}
+        ~scoped_connection()
+        {
+            this->disconnect();
+        }
+    };
+
+    enum connect_position { at_front, at_back };
+
+    template <typename R, typename... Args>
+    class signal;
+
+    template <typename R, typename... Args>
+    class signal<R(Args...)> : protected basic_signal<R(Args...)>
+    {
+        using base = basic_signal<R(Args...)>;
+    protected:
+        struct slot_info
+        {
+            int id;
+        };
+        std::vector<slot_info> slots_info;
+        int id_counter = 0;
+    public:
+        using base::operator();
+        using base::size;
+
+        template <typename F>
+        connection connect(F&& f, connect_position pos = at_back)
+        {
+            int i = id_counter;
+            if(pos == at_back)
+            {
+                this->push_back(std::forward<F>(f));
+                slots_info.push_back({i});
+            }
+            else
+            {
+                this->insert(this->begin(), std::forward<F>(f));
+                slots_info.insert(slots_info.begin(), {i});
+            }
+            id_counter++;
+            return connection{[this,i](){disconnect(i);}};
+        }
+
+    private:
+        void disconnect(int id) noexcept
+        {
+            assume(slots_info.size() == this->size());
+            int i;
+            for(i = 0; i < (int)slots_info.size(); i++)
+            {
+                if(slots_info[i].id == id)
+                    break;
+            }
+            assume(i >= 0 && i < (int)slots_info.size());
+            slots_info.erase(slots_info.begin() + i);
+            this->erase(this->begin() + i);
+        }
+
+    };
 
 }
 
