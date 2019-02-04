e76d784acf9d95fb6ecb0906afddbd0a3ed0494c
ftk <ftk@users.noreply.github.com>
2019-02-04 16:54:12 +0300

    boost system is header only starting with 1.69

--- external/boost.cpp
+++ external/boost.cpp
@@ -1,7 +1,11 @@
 // compile boost.system
+#include <boost/version.hpp>
+
+#if BOOST_VERSION < 106900
 #define BOOST_SYSTEM_SOURCE
 #include <boost/system/error_code.hpp>
 #include <boost/system/detail/error_code.ipp>
+#endif
 
 #ifdef BOOST_ASIO_SEPARATE_COMPILATION
 #include <boost/asio/impl/src.hpp>
