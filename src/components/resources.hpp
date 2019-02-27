8711e0714c940b671fdd3cc46c1032155bb59e36
ftk <ftk@users.noreply.github.com>
2019-02-27 14:29:44 +0300

    configurable texture cache

--- src/components/resources.hpp
+++ src/components/resources.hpp
@@ -10,30 +10,15 @@
 #include "opengl/texture.hpp"
 #include "opengl/textrender.hpp"
 
+#include "util/semiconfig.hpp"
 
-//#include "util/audio.hpp"
 
-
-/*
-#include "audio.h"
-
-template <>
-struct resource_traits<Sound>
-{
-    static unsigned int get_size(const Sound& rsc)
-    {
-        return 1;
-    }
-    static Sound from_id(string_view id) { return LoadSound(std::string{id}.c_str()); }
-};
-*/
 //=- register_component(class=>'resources_c', name=>'resources', priority=>50);
 
 class resources_c
 {
 public:
-    resource_cache<texture> textures{8 * 1024 * 1024};
-    //resource_cache<sound> sounds{8 * 1024 * 1024};
+    resource_cache<texture> textures{SCFG(cache.textures, 100 * 1024u)};
 
     font default_font { "resources/default.ttf" };
 };
